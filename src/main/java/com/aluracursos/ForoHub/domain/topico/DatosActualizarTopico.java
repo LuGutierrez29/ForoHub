ddb0667ccb7d0918e5ce95b2aa6cc21425e1d07a
Luis Alejandro Gutierrez Diaz <106104927+LuGutierrez29@users.noreply.github.com>
2024-07-16 04:18:18 -0500

    Creación de repositorio
    Primeros pasos

---
+++ src/main/java/com/aluracursos/ForoHub/domain/topico/DatosActualizarTopico.java
@@ -0,0 +1,8 @@
+package com.aluracursos.ForoHub.domain.topico;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DatosActualizarTopico(@NotNull Long id, String titulo, String mensaje,
+                                    String estatus, String curso) {
+
+}
---
+++ src/main/java/com/aluracursos/ForoHub/domain/topico/Estatus.java
@@ -0,0 +1,7 @@
+package com.aluracursos.ForoHub.domain.topico;
+
+public enum Estatus {
+
+    NO_solucionado,
+    SOLUCIONADO
+}
